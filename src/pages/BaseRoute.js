fc3d45c590997cad258070ff7bad535a2e60ef5d
ashwinbiju09 <20eucs018@skcet.ac.in>
2024-08-13 20:35:03 +0530

    Changes in BaseRoutes

--- src/pages/BaseRoute.js
+++ src/pages/BaseRoute.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Education from "./Education";
 import Experience from "./Experience";
@@ -7,14 +7,14 @@ import Projects from "./Projects";
 
 const BaseRoute = () => {
   return (
-    <>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/education" element={<Education />} />
         <Route path="/experience" element={<Experience />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
-    </>
+    </Router>
   );
 };
 
